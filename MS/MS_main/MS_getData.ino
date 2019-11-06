69c0bb20e390a88d201982707ec30f01faed81a8
DevicesSuai <56974148+DevicesSuai@users.noreply.github.com>
2019-11-06 21:29:03 +0300

    MS+eink
    Добавлен вывод на экран

--- MS/MS_main/MS_getData.ino
+++ MS/MS_main/MS_getData.ino
@@ -3,6 +3,11 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_SGP30.h>
+#include <ESP8266WiFi.h>
+#include <epd.h>
+
+const int wake_up = 5;
+const int reset = 4;
 
 #define BME_SCK D6
 #define BME_MISO D5
@@ -44,6 +49,10 @@ void setup_external(){
     while (1);
   }
   bme.begin();
+  epd_init(wake_up,reset);
+  epd_wakeup(wake_up);
+  epd_clear();
+  epd_set_memory(MEM_NAND);
 }
 
 uint32_t getAbsoluteHumidity(float temperature, float humidity) {
@@ -51,6 +60,31 @@ uint32_t getAbsoluteHumidity(float temperature, float humidity) {
     const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
     const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
     return absoluteHumidityScaled;
+}
+
+void draw_text(String t, String h, String c, String p)
+{
+  t = "Temperature = " + t;
+  char buf1[t.length()+1];
+  t.toCharArray(buf1,t.length()+1);
+  epd_disp_string(buf1, 50, 50);
+
+  h = "Humidity = " + h;
+  char buf2[h.length()+1];
+  h.toCharArray(buf2,h.length()+1);
+  epd_disp_string(buf2, 500, 50);
+
+  c = "CO2(ppm) = " + c;
+  char buf3[c.length()+1];
+  c.toCharArray(buf3,c.length()+1);
+  epd_disp_string(buf3, 50, 500);
+
+  p = "Pressure = " + p;
+  char buf4[p.length()+1];
+  p.toCharArray(buf4,p.length()+1);
+  epd_disp_string(buf4, 500, 500);
+  epd_udpate();
+  delay(5000);
 }
 
 void get_Data(){//получаем данные с датчиков
@@ -73,9 +107,9 @@ void get_Data(){//получаем данные с датчиков
     Serial.println("Raw Measurement failed");
     return;
   }
-  Serial.println();
-  delay(1000);
+  draw_text(String(Temp), String(Humidity), String(CO2), String(Pressure));
 }
+
 void send_Data(){//отправляем на mqtt (по запросу)
   mqtt_client.publish(top1,String(Temp));
   mqtt_client.publish(top2,String(Pressure));
